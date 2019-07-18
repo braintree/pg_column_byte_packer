053e7cbad3705ed4391d40be287470be1736bc58
jcoleman <james.coleman@getbraintree.com>
2019-07-18 17:17:59 +0000

    Pure refactor to extract ordering key from SchemaStatements
    Extracting this for reuse sets us up to be able to also support other
    paths to getting a byte-packed table (namely plans for re-ordering
    pg_dump output).

--- lib/pg_column_byte_packer.rb
+++ lib/pg_column_byte_packer.rb
@@ -1,7 +1,69 @@
 require "pg_column_byte_packer/version"
 
 module PgColumnBytePacker
-  # Your code goes here...
+  def self.sql_type_alignment_cache
+    @@sql_type_alignment_cache ||= {}
+  end
+
+  def self.ordering_key_for_column(connection:, name:, sql_type:, primary_key:, nullable:, has_default:)
+    alignment = PgColumnBytePacker.sql_type_alignment_cache[sql_type] ||=
+      case sql_type
+      when "bigint", "timestamp", /\Abigserial( primary key)?/
+        8 # Actual alignment for these types.
+      when "integer", "date", /\Aserial( primary key)?/
+        4 # Actual alignment for these types.
+      when "bytea"
+        # These types generally have an alignment of 4, but values of at most 127 bytes
+        # long they are optimized into 2 byte alignment.
+        # Since we'd expect any binary fields to be relatively long, we'll assume they
+        # won't fit into the optimized case.
+        4
+      when "text", "citext", "character varying"
+        # These types generally have an alignment of 4, but values of at most 127 bytes
+        # long they are optimized into 2 byte alignment.
+        # Since we don't have a good heuristic for determining which columns are likely
+        # to be long or short, we currently just slot them all after the columns we
+        # believe will always be long.
+        # If desired we could also differentiate on length limits if set.
+        3
+      when /\Acharacter varying\(\d+\)/
+        if (limit = /\Acharacter varying\((\d+)\)/.match(sql_type)[1])
+          if limit.to_i <= 127
+            2
+          else
+            4
+          end
+        end
+      when "smallint", "boolean"
+        2 # Actual alignment for these types.
+      else
+        if connection.select_value("SELECT typtype FROM pg_type WHERE typname = '#{sql_type}'") == "e"
+          4
+        elsif (typalign = connection.select_value("SELECT typalign FROM pg_type WHERE typname = '#{sql_type}'"))
+          case typalign
+          when "c"
+            0
+          when "s"
+            2
+          when "i"
+            4
+          when "d"
+            8
+          end
+        else
+          0
+        end
+      end
+
+    # Ordering components in order of most importance to least importance.
+    [
+      -alignment, # Sort alignment descending.
+      primary_key ? 0 : 1, # Sort PRIMARY KEY first.
+      nullable ? 1 : 0, # Sort NOT NULL first.
+      has_default && nullable ? 0 : 1, # Sort DEFAULT first (but only when also nullable).
+      name, # Sort name ascending.
+    ]
+  end
 end
 
 require "pg_column_byte_packer/schema_statements"
--- lib/pg_column_byte_packer/schema_statements.rb
+++ lib/pg_column_byte_packer/schema_statements.rb
@@ -3,10 +3,6 @@ require "active_record/migration"
 
 module PgColumnBytePacker
   module SchemaCreation
-    def self.sql_type_alignment_cache
-      @@sql_type_alignment_cache ||= {}
-    end
-
     def visit_TableDefinition(o)
       columns_hash = o.instance_variable_get(:@columns_hash)
 
@@ -19,63 +15,14 @@ module PgColumnBytePacker
           :primary_key => col.primary_key?,
         )
 
-        alignment = PgColumnBytePacker::SchemaCreation.sql_type_alignment_cache[sql_type] ||=
-          case sql_type
-          when "bigint", "timestamp", /\Abigserial( primary key)?/
-            8 # Actual alignment for these types.
-          when "integer", "date", /\Aserial( primary key)?/
-            4 # Actual alignment for these types.
-          when "bytea"
-            # These types generally have an alignment of 4, but values of at most 127 bytes
-            # long they are optimized into 2 byte alignment.
-            # Since we'd expect any binary fields to be relatively long, we'll assume they
-            # won't fit into the optimized case.
-            4
-          when "text", "citext", "character varying"
-            # These types generally have an alignment of 4, but values of at most 127 bytes
-            # long they are optimized into 2 byte alignment.
-            # Since we don't have a good heuristic for determining which columns are likely
-            # to be long or short, we currently just slot them all after the columns we
-            # believe will always be long.
-            # If desired we could also differentiate on length limits if set.
-            3
-          when /\Acharacter varying\(\d+\)/
-            if (limit = /\Acharacter varying\((\d+)\)/.match(sql_type)[1])
-              if limit.to_i <= 127
-                2
-              else
-                4
-              end
-            end
-          when "smallint", "boolean"
-            2 # Actual alignment for these types.
-          else
-            if @conn.select_value("SELECT typtype FROM pg_type WHERE typname = '#{sql_type}'") == "e"
-              4
-            elsif (typalign = @conn.select_value("SELECT typalign FROM pg_type WHERE typname = '#{sql_type}'"))
-              case typalign
-              when "c"
-                0
-              when "s"
-                2
-              when "i"
-                4
-              when "d"
-                8
-              end
-            else
-              0
-            end
-          end
-
-        # Ordering components in order of most importance to least importance.
-        [
-          -alignment, # Sort alignment descending.
-          col.options[:primary_key] ? 0 : 1, # Sort PRIMARY KEY first.
-          (case col.null when nil, true then 1 else 0 end), # Sort NOT NULL first.
-          col.default && (col.null || col.null.nil?) ? 0 : 1, # Sort DEFAULT first (but only when also nullable).
-          name, # Sort name ascending.
-        ]
+        PgColumnBytePacker.ordering_key_for_column(
+          connection: @conn,
+          name: name,
+          sql_type: sql_type,
+          primary_key: col.options[:primary_key],
+          nullable: col.null.nil? || col.null == true,
+          has_default: !col.default.nil?
+        )
       end
 
       columns_hash.clear
