42682cb76995e53095ba919aa0891c49781d89ae
jcoleman <james.coleman@getbraintree.com>
2019-07-24 20:58:52 +0000

    Standardize DB connection setup
    1. Use same env variable names as psql for convenience.
    2. Disable prepared statements since they don't work with the SQL
       history matcher gem (and because that matches our other apps).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,18 @@ require "bundler/setup"
 require "pg_column_byte_packer"
 require "relation_to_struct"
 require "db-query-matchers"
+require 'tempfile'
 
 ActiveRecord::Base.configurations = {
   "postgresql" => {
     "adapter" => 'postgresql',
-    "host" => ENV["PG_HOST"] || 'localhost',
-    "port" => ENV["PG_PORT"] || '5432',
+    "host" => ENV["PGHOST"] || 'localhost',
+    "port" => ENV["PGPORT"] || '5432',
     "database" => 'pg_column_byte_packer_tests',
     "encoding" => 'utf8',
-    "username" => ENV["PG_USER"] || `whoami`.strip,
-    "password" => ENV["PG_PASSWORD"],
+    "username" => ENV["PGUSER"] || `whoami`.strip,
+    "password" => ENV["PGPASSWORD"],
+    "prepared_statements" => false,
   },
 }
 
